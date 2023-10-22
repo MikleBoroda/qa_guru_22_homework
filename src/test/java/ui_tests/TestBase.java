2d8154fce43ee3366503272edf332c79b52c4571
Mihail Kuznecov <mikle.kuznecov@yandex.ru>
2023-10-22 23:52:49 +0300

    параметризованныt тесты на JUnit

--- src/test/java/ui_tests/TestBase.java
+++ src/test/java/ui_tests/TestBase.java
@@ -2,18 +2,12 @@ package ui_tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import pages.RegistrationPage;
-import pages.TextBoxPage;
 
 public class TestBase {
-    RegistrationPage registrationPage = new RegistrationPage();
-    TextBoxPage textBoxPage = new TextBoxPage();
-
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://www.svo.aero";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1680x1050";
     }
-
 }
